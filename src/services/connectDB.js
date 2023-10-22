fd158c46410d319f6810985c871ee04ad33f35c7
Timothy-py <adeyeyetimothy33@gmail.com>
2023-10-22 13:50:34 +0100

    ft<database>:setup mongoose database

---
+++ src/services/connectDB.js
@@ -0,0 +1,20 @@
+const { default: mongoose } = require("mongoose");
+
+const connectDB = async () => {
+  var DATABASE_URI = "";
+  const NODE_ENV = process.env.NODE_ENV;
+  if (NODE_ENV === "TEST") {
+    DATABASE_URI = "mongodb://127.0.0.1:27017/gallerytest";
+  } else if (NODE_ENV === "DEVELOPMENT") {
+    DATABASE_URI = "mongodb://127.0.0.1:27017/gallery";
+  } else {
+    DATABASE_URI = process.env.MONGODB_ATLAS_URI;
+  }
+
+  await mongoose.connect(DATABASE_URI);
+
+  const connection = mongoose.connection;
+  console.log("Connection to MongoDB Atlas established successfully");
+};
+
+module.exports = connectDB;
