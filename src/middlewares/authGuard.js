7d7d59b5f2e1948e4d10b3a158ba0153d83a1d57
Timothy-py <adeyeyetimothy33@gmail.com>
2023-10-23 00:10:47 +0100

    ft<auth guard>:implemented authentication guard middleware

---
+++ src/middlewares/authGuard.js
@@ -0,0 +1,43 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/userModel");
+
+// Authentication guard for routes
+const authGuard = async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    // set token from header
+    token = req.headers.authorization.split(" ")[1];
+  } else if (req.cookies.token) {
+    // set token from cookie
+    token = req.cookies.token;
+  }
+
+  // if token does not exist
+  if (!token)
+    return res
+      .status(401)
+      .json({ status: "fail", message: "Not authorized to access this route" });
+
+  const JWT_SECRET = process.env.JWT_SECRET;
+  try {
+    // verify token
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.user = await User.findById(decoded.user_id);
+    if (req.user) {
+      next();
+    } else {
+      return res
+        .status(404)
+        .json({ status: "fail", message: "User not found" });
+    }
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ status: "error", message: "Internal server error" });
+  }
+};
+
+module.exports = authGuard;
