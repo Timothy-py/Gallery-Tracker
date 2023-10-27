3a0bce67968d71cc1220776436e43b4fc764a60e
Timothy-py <adeyeyetimothy33@gmail.com>
2023-10-27 01:13:22 +0100

    ft<Image sorting>:implemented a periodic service function for sorting images and caching

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 const express = require("express");
+const cron = require('node-cron')
 
 const connectDB = require("./src/services/connectDB");
+const imgSort = require("./src/services/imgSort");
 const logger = require("./logger/logger");
 
 const app = express();
@@ -13,6 +15,7 @@ const companyRoutes = require("./src/routes/companyRoute");
 const userRoutes = require("./src/routes/userRoute");
 const imageRoutes = require("./src/routes/imageRoute");
 const eventRoutes = require("./src/routes/eventRoute");
+
 
 // MIDDLEWARES
 app.use(express.json());
@@ -26,6 +29,9 @@ app.use("/api/v1/events", eventRoutes);
 
 // CONNECT DATABASE
 connectDB();
+
+// LOAD CRON JOB
+cron.schedule('*/1 * * * *', imgSort)
 
 app.listen(APP_PORT, () => {
   logger.info(`Server is running on PORT: ${APP_PORT}`);
---
+++ src/services/imgSort.js
@@ -0,0 +1,33 @@
+const logger = require("../../logger/logger");
+const Company = require("../models/companyModel");
+const Image = require("../models/imageModel");
+const redis = require("./connectRedis");
+
+logger.info("Executing image sort function>>>>>>>>>>>>>>>");
+const imgSort = async () => {
+  try {
+    // get all the companies ID
+    const companies = await Company.find({}, "_id");
+
+    // sort the images associated with the company
+    for (let i = 0; i < companies.length; i++) {
+      const company = companies[i];
+      console.log(company)
+
+      const sorted = await Image.find({ _companyId: company._id }).sort({
+        "metadata.weight": -1,
+      });
+
+      // save to cache
+      await redis.set(`sortedImages_${company._id}`, JSON.stringify(sorted));
+    }
+
+    logger.info("Image sort function executed successfully>>>>>>>>>>>>>");
+    return true;
+  } catch (error) {
+    logger.error(`Error executing image sort function: >>>> ${error}`);
+    return false;
+  }
+};
+
+module.exports = imgSort;
