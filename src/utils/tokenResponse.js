c1f65af426de98ae127fadc3da60a354967ab5ca
Timothy-py <adeyeyetimothy33@gmail.com>
2023-11-06 20:30:45 +0100

    changes

--- src/utils/tokenResponse.js
+++ src/utils/tokenResponse.js
@@ -4,7 +4,7 @@ const sendTokenResponse = (model, res) => {
 
   const options = {
     expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60
     ),
     httpOnly: true,
   };
